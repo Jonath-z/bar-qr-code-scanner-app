da63a4d644bff3db49f9f91a0a1b2077bd5d5100
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-15 17:43:02 +0200

    error handled

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -5,82 +5,29 @@ const HomePage = () => {
   const [data, setData] = useState<any>();
   const [stopStream, setStopStream] = useState(false);
   const [isScannerModal, setIsScannerModal] = useState(false);
-  const [scannerSize, setscannerSize] = useState({
-    height: 300,
-  });
-
-  const [isCodebar, setIscodebar] = useState(true);
-  const [isQr, setIsQr] = useState(false);
 
   const toggleScannerModal = () => {
     setStopStream(!stopStream);
     setIsScannerModal(!isScannerModal);
   };
 
-  const selectCodebar = () => {
-    setscannerSize({ ...scannerSize, height: 300 });
-    setIscodebar(true);
-    setIsQr(false);
-  };
-
-  const selectQrcode = () => {
-    setscannerSize({ ...scannerSize, height: 500 });
-    setIscodebar(false);
-    setIsQr(true);
-  };
-
-  const onSelectCodeType = (code: string) => {
-    if (code === "codebar") {
-      selectCodebar();
-    } else if (code === "qrcode") {
-      selectQrcode();
-    }
-  };
-
   const onError = (error: any) => {
     if (error.name === "NotAllowedError") {
-      // Handle messaging in our app after the user chooses to not allow the camera permissions
+      window.alert(
+        "We don't have access to the camera. Allow access to the camera"
+      );
     }
   };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-5 mx-5 font-bold lg:mx-96">
-      <div
-        className={`w-full flex justify-around items-center absolute top-0 mt-10 ${
-          isScannerModal && "hidden"
-        }`}
-      >
-        <button
-          className={`border border-blue-500 px-5 py-2 rounded-3xl ${
-            isCodebar ? "bg-blue-500 text-white " : "bg-white"
-          } transition-all`}
-          onClick={() => onSelectCodeType("codebar")}
-        >
-          Barcode
-        </button>
-        <button
-          className={`border border-blue-500 px-5 py-2 rounded-3xl ${
-            isQr ? "bg-blue-500 text-white" : "bg-white"
-          } transition-all`}
-          onClick={() => onSelectCodeType("qrcode")}
-        >
-          QR code
-        </button>
-      </div>
       <p
-        className={`bg-blue-500 text-white  py-2 rounded-sm w-[90%] text-center absolute top-0 mt-10 ${
-          !isScannerModal && "hidden"
-        }`}
+        className={`bg-blue-500 text-white  py-2 rounded-sm w-[90%] text-center absolute top-0 mt-10`}
       >
-        {isScannerModal && isCodebar
-          ? "Codebar scanning"
-          : isScannerModal && isQr && "QR code scanning"}
+        Scan Barcode and QR code
       </p>
-      {/* <div className="w-full h-28 border rounded-sm"> */}
       {isScannerModal && (
         <BarcodeScannerComponent
-          // width={500}
-          height={200}
           onUpdate={(err, result) => {
             if (result) {
               setData(result.getText());
@@ -92,8 +39,6 @@ const HomePage = () => {
           onError={onError}
         />
       )}
-      {/* </div> */}
-
       <p className="text-blue-500">{data}</p>
 
       <button
