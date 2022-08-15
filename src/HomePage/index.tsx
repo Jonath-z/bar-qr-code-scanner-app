8409ad179f865ee7144081043d619e33b7af187f
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-15 16:40:34 +0200

    fixed width

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -73,7 +73,8 @@ const HomePage = () => {
       <div className="w-fit h-fit border rounded-sm">
         {isScannerModal && (
           <BarcodeScannerComponent
-            height={isCodebar ? 200 : 500}
+            width={500}
+            height={200}
             onUpdate={(err, result) => {
               if (result) {
                 setData(result.getText());
